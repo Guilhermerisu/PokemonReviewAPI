f0c02b4a6b3245bc6eaf9341fcbcca17ab6342d9
Guilherme Pimenta <guilherme.pimenta@impar.com.br>
2022-10-19 19:43:29 -0300

    PUT method in Reviews

--- Interfaces/IReviewRepository.cs
+++ Interfaces/IReviewRepository.cs
@@ -9,6 +9,7 @@ namespace C_Comfy.Interfaces
         ICollection<Review> GetReviewsOfAPokemon(int pokeId);
         bool ReviewExists(int reviewId);   
         bool CreateReview(Review review);
+        bool UpdateReview(Review review);
         bool Save();
     }
 }
